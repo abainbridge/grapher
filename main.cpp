7270ad16be52de860a08f70c97bc3763f86ad045
Andrew Bainbridge <andy@rfcreations.com>
2024-05-17 18:57:58 +0100

    Output PNG instead of BMP. Tidy code.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include "df_bmp.h"
+// Deadfrog headers
 #include "df_common.h"
-#include "df_font.h"
 #include "df_window.h"
 #include "fonts/df_mono.h"
+
+// Contrib headers
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+// Standard headers
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -19,8 +24,10 @@ struct Axis {
 };
 
 
-void DrawChart(DfBitmap* bmp, double* xVals, unsigned numXVals, double* yVals, unsigned numYVals,
+void DrawChart(DfBitmap* bmp, std::vector<double> &xVals, std::vector<double> &yVals,
                Axis* xAxis, Axis* yAxis, DfColour colour) {
+    ReleaseAssert(xVals.size() == yVals.size(), "xVals and yVals must have same size");
+
     int chartW = bmp->width * 0.7;
     int chartH = bmp->height * 0.8;
     int chartX = bmp->width * 0.2;
@@ -56,13 +63,36 @@ void DrawChart(DfBitmap* bmp, double* xVals, unsigned numXVals, double* yVals, u
     // Draw data
     int prevX = 0;
     int prevY = 0;
-    for (unsigned i = 0; i < numXVals; i++) {
+    for (unsigned i = 0; i < xVals.size(); i++) {
         double x = (xVals[i] - xAxis->low) * xScale + chartX;
         double y = (yVals[i] - yAxis->low) * yScale + chartY;
-        if (i) DrawLine(bmp, prevX, prevY, x, y, colour);
+//        if (i) DrawLine(bmp, prevX, prevY, x, y, colour);
+        RectFill(bmp, x-1, y-1, 3, 3, colour);
         prevX = x;
         prevY = y;
     }
+}
+
+
+static void SwapRedAndBlue(DfBitmap* bmp) {
+    DfColour* pixel = bmp->pixels;
+    for (int y = 0; y < bmp->height; y++) {
+        for (int x = 0; x < bmp->width; x++) {
+            unsigned char tmp = pixel->r;
+            pixel->r = pixel->b;
+            pixel->b = tmp;
+            pixel++;
+        }
+    }
+}
+
+
+static void SaveBitmap(DfBitmap *bmp, char const *filename) {
+    SwapRedAndBlue(bmp);
+    ReleaseAssert(stbi_write_png(filename, bmp->width,
+            bmp->height, 4, bmp->pixels, bmp->width * sizeof(DfColour)), 
+        "Couldn't save '%s'", filename);
+    SwapRedAndBlue(bmp);
 }
 
 
@@ -76,45 +106,39 @@ int main()
     g_window = CreateWin(1400, 1100, WT_WINDOWED_RESIZEABLE, "Grapher");
     g_defaultFont = LoadFontFromMemory(df_mono_9x18, sizeof(df_mono_9x18));
 
-    FILE* f = fopen("c:/work/sniffer/src/android/src/log_difference_after.txt", "rb");
+    //
+    // Parse the log file.
+
+    FILE* f = fopen("C:/work/sniffer/src/android/src/log_difference_after.txt", "rb");
     ReleaseAssert(f, "Couldn't open log");
 
-    // Log lines look like:
-    // 05-09 09:53:26.697  6344  6372 D mini_moreph_capture: wuz phone_time 1715244806.697 difference 0.367 rtt 0.602
     std::vector <double> phone_time;
     std::vector <double> difference;
     std::vector <double> rtt;
 
     char line[200];
     while (fgets(line, sizeof(line), f)) {
-        char *phoneTimeStr = strstr(line, "phone_time");
-        char* differenceStr = strstr(line, "difference");
-        char* rttStr = strstr(line, "rtt");
+        char *phoneTimeStr = strstr(line, "phone_time ");
+        char* differenceStr = strstr(line, "difference ");
+        char* rttStr = strstr(line, "rtt ");
         if (!phoneTimeStr || !differenceStr || !rttStr) continue;
 
         phoneTimeStr += 11;
         differenceStr += 11;
         rttStr += 4;
 
-        phone_time.push_back(atof(phoneTimeStr));
+        phone_time.push_back(atof(phoneTimeStr) - 1715244812.723);
         difference.push_back(atof(differenceStr));
         rtt.push_back(atof(rttStr));
     }
 
-    //{
-    //    Axis xAxis = { 1715260890.0, 1715260930.0, 5, "%.0f", "Phone time", "seconds" };
-    //    Axis yAxis = { 0.0, 2.0, 0.2, "%.1f", "Round trip time", "milliseconds" };
-    //    DfColour colour = Colour(128, 20, 20);
-    //    DrawChart(g_window->bmp, phone_time.data(), phone_time.size(), rtt.data(), rtt.size(), &xAxis, &yAxis, colour);
-    //    SaveBmp(g_window->bmp, "../../rtt.bmp");
-    //}
-
+    // Draw the chart.
     {
-        Axis xAxis = { 1715260890.0, 1715260930.0, 5, "%.0f", "Phone time", "seconds" };
-        Axis yAxis = { 0.0, 3.0, 0.5, "%.1f", "Time delta", "milliseconds" };
-        DfColour colour = Colour(20, 20, 170);
-        DrawChart(g_window->bmp, phone_time.data(), phone_time.size(), difference.data(), difference.size(), &xAxis, &yAxis, colour);
-        SaveBmp(g_window->bmp, "../../difference.bmp");
+       Axis xAxis = { 0.6, 3.0, 0.2, "%.1f", "Phone time - fpga time", "milliseconds" };
+       Axis yAxis = { 0.0, 2.2, 0.2, "%.1f", "Round trip time", "milliseconds" };
+       DfColour colour = Colour(128, 20, 20);
+       DrawChart(g_window->bmp, difference, rtt, &xAxis, &yAxis, colour);
+       SaveBitmap(g_window->bmp, "../../second_difference_vs_rtt.png");
     }
 
     while (!g_window->windowClosed && !g_window->input.keys[KEY_ESC]) {
